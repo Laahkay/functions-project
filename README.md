566cca881f5b39d1e04c40d73725cfb87780c68c
Laahkay <lonerkiddow15@gmail.com>
2022-05-26 11:32:29 +0200

    adding functionality

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # functions-project
+
+# 1 given an array of names return the longest name (['bob' , 'smith', 'longman']) longman. 
+I have use a loop in order to get the longest name in an array and as i have done that i have done a console on it
+and it returns the longest name which is kgothatso
+
+
+# given a function that passes two values value1 and value2 calculate the two values and return the answer useing this values(1 , 2) answer 3
+
+it is where i have to create a function calculateValues so thta  i can get the total sum of my calculation which is 3 when we are adding 1 ,2 the total should
+give us 3
+
+# given an array of values return the total amount. use this array of numbers([1 , 2 ,3 ]) 6
+
+here i have created a function where by i made a list og the list of arrays to calulate and i have used a loop to that kind of calculation
+
+
+# given a value, you should be able to store the values in an array  
---
+++ basic.js
@@ -0,0 +1,55 @@
+// First answer
+ const words = ['bob','Alicia','Kgothatso']
+
+ const getLongestWord = (word) => {
+    let longestWord = word.sort((a,b) => a.length - b.length).reverse();
+    return longestWord[0]
+}
+ console.log(getLongestWord(words))
+
+ //  second Quation
+ const array = [1, 2, 3]; 
+ 
+ const calculateList = (list) => {
+ 
+ let sum = 0;
+ 
+ for (let i = 0; i < array. length; i++) { sum += array[i];
+ }
+ 
+ return sum
+ 
+ }
+ 
+ console.log(calculateList(array))
+
+
+// Third answer
+
+const calculateValues = (num1 , numb2) => {
+    const addedValues = num1 + numb2
+    
+    return addedValues 
+    }
+    
+    console.log(calculateValues (1 , 2))
+
+
+// fourth answer
+//     const arr1 = [{name:"moral" , surname:"smith"} , {name:"thabo" , surname:"king"}];
+    
+//     for( let i = 0; i < arr1.length; i++);{
+
+//         newArr.push(arr1[i].name);
+//     }
+// console.log()
+function Names(){
+    let people = [{name:"moral" , surname:"smith"} , {name:"thabo" , surname:"king"}]
+    let newArray = [];
+    for( let i = 0; i < people.length; i++){
+
+        newArry.push(people[i].name);
+    }
+    return newArray
+}
+console.log(Names())
